2dabb41b7568f990f7e58499acea69c4eae1ee91
Amy McCabe <mcca0168@umn.edu>
2022-08-31 14:21:58 -0500

    lesson 2, images, css, plugins

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,18 @@
 import "./form.js";
 import "./submit.js";
+
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../css/index.css";
+
+//import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+import Bicycle from '../images/bicycle.png';
+
+window.addEventListener('load', function () {
+  //document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+  this.document.getElementById('logo').src = Bicycle;
+});
