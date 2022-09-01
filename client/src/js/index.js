8a58940d4af592c8c3343f597ea08ff39e54202d
Amy McCabe <mcca0168@umn.edu>
2022-09-01 13:10:28 -0500

    GET and POST to IndexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,10 +10,13 @@ import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 import Bicycle from '../images/bicycle.png';
 
-import { initDb } from './database';
+import { initDb, getDb, postDb } from './database';
 
 window.addEventListener('load', function () {
   initDb();
+  getDb();
+  postDb('McAmy2001', 'email@email.com', 6126126126, 'Bear');
+  getDb();
   //document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
